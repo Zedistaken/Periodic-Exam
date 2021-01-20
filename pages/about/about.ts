3d33d65bca152c600173fa3df799425285f4ada1
Zedistaken <zachary.nabong10@gmail.com>
2021-01-20 09:47:45 +0800

    1 file modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -3,7 +3,8 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
-  templateUrl: 'about.html'
+  templateUrl: 'about.html',
+
 })
 export class AboutPage {
   feet: number;
